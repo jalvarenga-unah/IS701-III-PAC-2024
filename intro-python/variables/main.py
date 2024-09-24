6e972e8894a94adb0fa4519e354be27cb68371a1
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-09-24 09:32:58 -0600

    introducción a python

---
+++ intro-python/variables/main.py
@@ -0,0 +1,15 @@
+# se puede "tipar los datos, pero no es restrictivo"
+nombre : str = 'Juan'
+edad = 3
+
+# None, para declarar variables sin un valor inicial
+apellido = None
+
+# formatedor de cadenas
+print(f'Hola {nombre} ')
+
+# función "len", obtiene el tamaño de un iterable
+print(nombre[ len(nombre) - 1 ])
+
+# La función "list", permite crear una nueva lista con los calores enviados, o si es un string, caracter por caracter
+print(list(nombre))
